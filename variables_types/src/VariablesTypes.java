13acf9a0f96bfc180e8849b43aa4b7136e173d3b
Wallace Oliveira Wanderlei Dias <wallacedias0711@gmail.com>
2025-05-01 19:01:53 -0300

    Commit exercises

--- variables_types/src/VariablesTypes.java
+++ variables_types/src/VariablesTypes.java
@@ -1,5 +1,35 @@
+/* PrimitiveType 
+ * byte - Valor -128 a 127 podemos usar 1 byte
+ * short - Valor -32.768 a 32.767 podemos usar 2 bytes
+ * int - Valor -122147483648 a 2147483647 podemos usar 4 bytes
+ * long - Valor -9223372036854775808L a 9223372036854775807L podemos usar 8 bytes
+ 
+ * Tipos primitivos que podem conter partes fracionarias:
+
+ * float - usa 4 bytes de memoria
+ * double - usa 8 bytes de memoria
+ 
+
+ * boolean - True or false
+ * char - caracter
+ 
+ Operadores
+
+ == 'Igual'
+ != 'Diferente'
+ < 'Menor'
+ > 'Maior'
+ <= 'Menor Igual'
+ >= 'Maior Igual'
+  
+ * 
+*/
+
 public class VariablesTypes {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
     }
 }
